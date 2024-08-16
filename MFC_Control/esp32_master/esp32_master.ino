1e6623d7546ebc966ce04ccafb3e750caa54ea74
HeegonChae <113625699+HeegonChae@users.noreply.github.com>
2024-08-17 00:49:14 +0900

    feat(esp32_master.ino) : match the latest version

--- MFC_Control/esp32_master/esp32_master.ino
+++ MFC_Control/esp32_master/esp32_master.ino
@@ -3,27 +3,8 @@
 #include <WiFiClient.h>
 #include <WiFiServer.h>
 
-// const char* ssid = "TOM N TOMS 2G";
-
-// const char* ssid = "902";
-// const char* password = "22177070";
-
-// const char* ssid = "addinedu_class_1(2.4G)";
-// const char* password = "addinedu1";
-
-// const char* ssid = "김미꾜";
-// const char* password = "01033616648";
-
-// const char* ssid = "CLL5G";
-// const char* password = "cll16661140";
-
-
-const char* ssid = "Tmwlakfk_>u<";
-const char* password = "96989898";
-
-// const char* ssid = "FOREST_GB_2G";
-// const char* password = "forest1234";
-
+const char* ssid = "addinedu_class_2 (2.4G)";
+const char* password = "addinedu1";
 
 #define SLAVE1_ADDR 0x08
 #define SLAVE2_ADDR 0x09
@@ -33,22 +14,8 @@ WiFiServer server(80); // TCP 서버 포트 설정
 unsigned long previousMillis = 0; // 이전 시간 저장
 const long interval = 1000; // 1초 간격
 
-// String MFCNetworkManagerIP = "172.30.1.28"; // network_manager IP 주소 탐탐
-// String MFCNetworkManagerIP = "192.168.0.15"; // network_manager IP 주소 쬰지네
-// String MFCNetworkManagerIP = "192.168.2.28"; // network_manager IP 주소 학원172.20.10.8
-
-// String MFCNetworkManagerIP = "172.20.10.8"; // network_manager IP 주소 학원
-String MFCNetworkManagerIP = "192.168.1.104"; // network_manager IP 주소 학원>3<
-
-// String MFCNetworkManagerIP = "192.168.0.11"; // network_manager IP 주소 개방~
+String MFCNetworkManagerIP = "192.168.0.89"; // network_manager IP 주소
 const uint16_t networkManagerPort = 12345;
-
-
-// String slave3IP = "192.168.2.84"; // 슬레이브 3 IP 주소(ESP32 RACK LED) 학원
-String slave3IP = "192.168.1.106"; // 슬레이브 3 IP 주소(ESP32 RACK LED) >ㅁ<
-
-// String slave3IP = "192.168.0.15"; // 슬레이브 3 IP 주소(ESP32 RACK LED)개방
-const uint16_t slave3Port = 80; // 슬레이브 보드의 서버 포트
 
 void setup() {
   Serial.begin(9600);
@@ -60,7 +27,6 @@ void setup() {
   Wire.begin(21, 22); // I2C 마스터 초기화, SDA=GPIO 21, SCL=GPIO 22
 
   WiFi.begin(ssid, password);
-  // WiFi.begin(ssid);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Connecting to WiFi...");
@@ -88,66 +54,18 @@ void handleClientRequest() {
         String request = client.readStringUntil('\n');
         Serial.println("Received request: " + request);
         if (request.startsWith("START_INSPECTION:")) {
-          int firstSeparator = request.indexOf(':');
-          int secondSeparator = request.indexOf(':', firstSeparator + 1);
-          
-          if (firstSeparator != -1 && secondSeparator != -1) {
-            String productCode = request.substring(firstSeparator + 1, secondSeparator);
-            String quantity = request.substring(secondSeparator + 1);
-            Serial.println("Received start inspection signal for product: " + productCode + " with quantity: " + quantity);
-            sendCommandToArduino(SLAVE1_ADDR, "START_INSPECTION:" + productCode + ":" + quantity);
-            // sendCommandToArduino(SLAVE2_ADDR, "START_INSPECTION:" + productCode + ":" + quantity);
+          String productCode = request.substring(request.indexOf(':') + 1);
+          Serial.println("Received start inspection signal for product: " + productCode);
+          sendCommandToArduino(SLAVE1_ADDR, "LED_ON:" + productCode);
+          sendCommandToArduino(SLAVE2_ADDR, "LED_ON:" + productCode);
 
-            client.println("Inspection started for product: " + productCode + " with quantity: " + quantity);
-          }
+          client.println("Inspection started for product: " + productCode);
         }
-        
-        if (request.startsWith("START-입고:")) {
-          int firstSeparator = request.indexOf('[');
-          int secondSeparator = request.indexOf(']', firstSeparator + 1);
-          
-          if (firstSeparator != -1 && secondSeparator != -1) {
-            String rackList = request.substring(firstSeparator + 1, secondSeparator);
-            rackList.trim();
-            Serial.println("Received rack list: " + rackList);
-            rackList.replace("'", ""); // 따옴표 제거
-            sendCommandToSlave(slave3IP, rackList);
-
-            client.println("Inspection started for racks: " + rackList);
-          }
-        }
+        // 다른 요청을 처리하기 위한 조건문을 추가하세요.
       }
     }
     client.stop();
     Serial.println("Client disconnected");
-  }
-}
-void sendCommandToSlave(String slaveIP, String rackList) {
-  WiFiClient client;
-  if (client.connect(slaveIP.c_str(), slave3Port)) {
-    // 랙 리스트를 ','로 분할하여 각각에 "I,S"를 추가하여 전송
-    int start = 0;
-    int end = rackList.indexOf(',');
-    while (end != -1) {
-      String rack = rackList.substring(start, end);
-      rack.trim();
-      String command = rack + ",I,S";
-      client.println(command);
-      Serial.println("Sent command to slave: " + command);
-      start = end + 1;
-      end = rackList.indexOf(',', start);
-    }
-    // 마지막 랙 처리
-    String rack = rackList.substring(start);
-    rack.trim();
-    String command = rack + ",I,S";
-    client.println(command);
-    Serial.println("Sent command to slave: " + command);
-
-    delay(1000); // 모든 명령이 전송될 때까지 기다림
-    client.stop();
-  } else {
-    Serial.println("Connection to slave failed: " + slaveIP);
   }
 }
 
@@ -158,6 +76,7 @@ void checkInspectionComplete() {
 
     // 슬레이브로부터 검사 완료 데이터를 요청
     requestInspectionComplete(SLAVE1_ADDR);
+    requestInspectionComplete(SLAVE2_ADDR);
   }
 }
 
@@ -166,6 +85,7 @@ void sendCommandToArduino(uint8_t address, String command) {
   Wire.write((const uint8_t*)command.c_str(), command.length() + 1); // null terminator 포함
   Wire.endTransmission();
 }
+
 
 void requestInspectionComplete(uint8_t address) {
   Wire.requestFrom(address, 33); // null terminator 포함 33 바이트의 데이터를 요청 (예: 최대 메시지 길이)
@@ -182,18 +102,17 @@ void requestInspectionComplete(uint8_t address) {
   String responseStr = String(response);
   if (responseStr.startsWith("INSPECTION_COMPLETE:")) {
     String productCode = responseStr.substring(responseStr.indexOf(':') + 1);
-    Serial.println("Inspection completed for product:" + productCode);
+    Serial.println("Inspection completed for product: " + productCode);
     // 필요한 추가 작업 수행
-    notifyNetworkManager("Inspection",productCode);
+    notifyNetworkManager(productCode);
   }
 }
 
-void notifyNetworkManager(const String& task,const String& productCode) {
+void notifyNetworkManager(const String& productCode) {
   WiFiClient client;
   Serial.println("Attempting to connect to network manager...");
   if (client.connect(MFCNetworkManagerIP.c_str(), networkManagerPort)) {
-    String message = "result-"+task +":" + productCode;
-    Serial.println(productCode);
+    String message = "TASK_COMPLETE:" + productCode;
     client.print(message);
     client.stop();
     Serial.println("Sent task completion to network_manager: " + message);
